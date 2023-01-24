5348cbb177c2b9547eafd43e0fccc163d689137a
Marco Boss <bossm8@hotmail.com>
2023-01-24 18:51:58 +0000

    various adjustments, remove woffs too from source-control

--- .devcontainer/download.sh
+++ .devcontainer/download.sh
@@ -1,23 +1,26 @@
 #!/bin/bash
 
 OUTPUT_DIR=$(dirname "$0")/../static/
-BOOTSTRAP=/tmp/gofolio-bootstrap.zip
-BOOTSTRAP_ICONS=/tmp/gofolio-bootstrap-icons.zip
-BOOTSTRAP_U=/tmp/gofolio-bootstrap
-BOOTSTRAP_ICONS_U=/tmp/gofolio-bootstrap-icons
+BOOTSTRAP=/tmp/portfoli.go-bootstrap.zip
+BOOTSTRAP_ICONS=/tmp/portfoli.go-bootstrap-icons.zip
+BOOTSTRAP_U=/tmp/portfoli.go-bootstrap
+BOOTSTRAP_ICONS_U=/tmp/portfoli.go-bootstrap-icons
+ANIME_JS=/tmp/portfoli.go-anime.zip
+ANIME_JS_U=/tmp/portfoli.go-anime
 
 wget https://github.com/twbs/bootstrap/releases/download/v5.0.2/bootstrap-5.0.2-dist.zip -O ${BOOTSTRAP}
 wget https://github.com/twbs/icons/releases/download/v1.10.3/bootstrap-icons-1.10.3.zip -O ${BOOTSTRAP_ICONS}
+wget https://github.com/juliangarnier/anime/archive/refs/tags/v3.2.1.zip -O ${ANIME_JS}
 
-unzip ${BOOTSTRAP} -d ${BOOTSTRAP_U}
-mv ${BOOTSTRAP_U}/*/* ${BOOTSTRAP_U}
-unzip ${BOOTSTRAP_ICONS} -d ${BOOTSTRAP_ICONS_U}
-mv ${BOOTSTRAP_ICONS_U}/*/* ${BOOTSTRAP_ICONS_U}
+for ZIP in ${BOOTSTRAP} ${BOOTSTRAP_ICONS} ${ANIME_JS}; do
+    unzip ${ZIP} -d ${ZIP%.*}
+    mv ${ZIP%.*}/*/* ${ZIP%.*}
+done
 
+test -d ${OUTPUT_DIR}/css/fonts || mkdir ${OUTPUT_DIR}/css/fonts
 cp ${BOOTSTRAP_U}/css/bootstrap.min.css ${OUTPUT_DIR}/css/
 cp ${BOOTSTRAP_U}/js/bootstrap.min.js ${OUTPUT_DIR}/js/
 cp ${BOOTSTRAP_ICONS_U}/bootstrap-icons.css ${OUTPUT_DIR}/css
-test -d ${OUTPUT_DIR}/css/fonts || mkdir ${OUTPUT_DIR}/css/fonts
 cp ${BOOTSTRAP_ICONS_U}/fonts/bootstrap-icons.woff* ${OUTPUT_DIR}/css/fonts
 
-rm -rf /tmp/gofolio-bootstrap*
+rm -rf /tmp/portfoli.go-*
--- static/js/form-validation.js
+++ static/js/form-validation.js
@@ -1,16 +1,41 @@
 (function () {
     var forms = document.querySelectorAll('.needs-validation');
-    Array.prototype.slice.call(forms).forEach((form) => {
-        form.addEventListener(
-            'submit', 
-            function (event) {
-                if (!form.checkValidity()) {
-                    event.preventDefault()
-                    event.stopPropagation()
-                }
-                form.classList.add('was-validated')
-            }, 
-            false
-        );
-    })
+    forms.forEach(f => addValidationListener(f))
 })();
+
+function addValidationListener(form) {
+    form.addEventListener(
+        'submit', 
+        function (event) {
+            if (!form.checkValidity()) {
+                event.preventDefault()
+                event.stopPropagation()
+            }
+            form.classList.add('was-validated')
+        }, 
+        false
+    );
+}
+
+const button = document.getElementById("runaway-btn");
+
+const animateMove = (element, prop, pixels) =>
+  anime({
+    targets: element,
+    [prop]: `${pixels}px`,
+    easing: "easeOutCirc"
+  });
+
+["mouseover", "click"].forEach(function (el) {
+  button.addEventListener(el, function (event) {
+    const top = getRandomNumber(window.innerHeight - this.offsetHeight);
+    const left = getRandomNumber(window.innerWidth - this.offsetWidth);
+
+    animateMove(this, "left", left).play();
+    animateMove(this, "top", top).play();
+  });
+});
+
+const getRandomNumber = (num) => {
+  return Math.floor(Math.random() * (num + 1));
+};
