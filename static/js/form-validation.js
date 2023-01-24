cbcd5a7c4edd6247fd8902cf9d2aff464a456156
Marco Boss <bossm8@hotmail.com>
2023-01-24 20:06:15 +0000

    runnaway button works

--- static/js/form-validation.js
+++ static/js/form-validation.js
@@ -1,49 +1,38 @@
 (function () {
-    var forms = document.querySelectorAll('.needs-validation');
-    forms.forEach(f => addValidationListener(f));
-})();
-
-function addValidationListener(form) {
-    form.addEventListener(
-        'submit', 
-        function (event) {
-            if (!form.checkValidity()) {
-                event.preventDefault()
-                event.stopPropagation()
-                makeButtonRun(true);
-            } else {
-                makeButtonRun(false);
-            }
-            form.classList.add('was-validated')
-        }, 
-        false
-    );
-}
-
-const randInt = (max) => {
-  return Math.floor(Math.random() * (max + 1));
-};
-
-function makeButtonRun(runAway) {
-    console.log(runAway)
+    const form = document.getElementById('contact-form');
     const button = document.getElementById("runaway");
-    if (run) {
-        button.addEventListener('mouseover', run);
-        button.addEventListener('click', run)
-    } else {
-        button.removeEventListener('mouseover', run);
-        button.removeEventListener('click', run);
+
+    form.addEventListener('submit', validate);
+
+    function validate(event) {
+        if (!form.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+            button.addEventListener('mouseover', run);
+            button.addEventListener('click', run);
+        }
+        this.classList.add('was-validated')
     }
 
-}
+    const randInt = (max) => {
+      return Math.floor(Math.random() * (max + 1));
+    };
 
-function run() {
-    console.log(this)
-    const left = randInt(window.innerWidth - this.offsetWidth);
-    console.log(left)
-    anime({
-      targets: this,
-      ['left']: `${left}px`,
-      easing: "easeOutCirc"
-    }).play();
-}
+    function run() {
+        if (form.checkValidity()) {
+            button.removeEventListener('click', run);
+            button.removeEventListener('mouseover', run);
+            return
+        }
+        console.log(button.parentElement.clientWidth)
+        const left = randInt(button.parentElement.clientWidth - this.offsetWidth);
+        anime({
+          targets: this,
+          ['left']: `${left}px`,
+          easing: "easeOutCirc"
+        }).play();
+    }
+
+})();
+
+
